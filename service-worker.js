0c5f3038e68bce4ce06bf7ed50a1d95a4638b981
yowbhergie2 <yowbhergie2@gmail.com>
2025-11-14 00:58:01 +0800

    Initial commit - DPWH Vehicle Management System

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'fuel-app-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
